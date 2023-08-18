cba9e18ca47975e13b56e1ad736ba1397d58a7a6
hariprakash-sparkout <hariprakash@sparkouttech.com>
2023-08-18 16:52:28 +0530

    fixes

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -233,8 +233,6 @@ export class AuthService {
 
     const otp = Math.floor(Math.random() * (max - min + 1) + min);
 
-    user.otp = otp;
-    
     const postData = {
       number: number,
       message: `Otp only valid for 45sec : ${otp}`,
@@ -242,6 +240,7 @@ export class AuthService {
     if(user.otp !=null){
       throw new NotAcceptableException('Please wait 45 seconds and try again')
     }
+    user.otp = otp;
     await user.save();
     setTimeout(async () => {
       user.otp = null;
