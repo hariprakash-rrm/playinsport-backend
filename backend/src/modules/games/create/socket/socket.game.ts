ff90c94f095cec9f0f17b3eaedeb07fa3782b9fc
hariprakash-sparkout <hariprakash@sparkouttech.com>
2023-08-31 23:20:42 +0530

    env

--- backend/src/modules/games/create/socket/socket.game.ts
+++ backend/src/modules/games/create/socket/socket.game.ts
@@ -10,7 +10,7 @@ import { env } from 'process';
 import { AuthService } from 'src/modules/auth/auth.service';
 require("dotenv").config();
 
-@WebSocketGateway({ cors: { origin: ['http://teamquantum.in','http://www.teamquantum.in'] } })
+@WebSocketGateway({ cors: { origin: ['http://teamquantum.in','http://www.teamquantum.in','https://playinsport.com','https://www.playinsport.com'] } })
 export class GameGateWay implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private authService:AuthService,@InjectModel(User.name)
   private userModels: Model<User>, @InjectModel(Game.name) private gameModels: Model<Game>, @InjectModel(GameDetails.name) private gameDeatilsModel: Model<GameDetails>
